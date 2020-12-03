776693a8002f3efab1f50e20ef2ec10098ab1598
Nam Vũ <namthp99@gmail.com>
2020-12-03 12:01:42 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the data, converters convert the letter to a number
+data= np.loadtxt('letter-recognition.data', dtype= 'float32', delimiter = ',',
+                    converters= {0: lambda ch: ord(ch)-ord('A')})
+
+# split the data to two, 10000 each for train and test
+train, test = np.vsplit(data,2)
+
+# split trainData and testData to features and responses
+responses, trainData = np.hsplit(train,[1])
+labels, testData = np.hsplit(test,[1])
+
+# Initiate the kNN, classify, measure accuracy.
+knn = cv2.ml.KNearest_create()
+knn.train(trainData, 0, responses)
+
+ret, result, neighbours, dist = knn.findNearest(testData[1].reshape(-1,16), k=5)
+
+print(testData[1])
+print({0: lambda ch: ord(ch)-ord('A')})
+#correct = np.count_nonzero(result == labels)
+#accuracy = correct*100.0/10000
+#print(accuracy)
